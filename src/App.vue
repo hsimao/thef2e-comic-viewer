48aac56185c308130e681efea88b9af8f90fca8a
hsimao <e087754958@gmail.com>
2018-07-07 22:29:17 +0800

    dev init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+
+</script>
+
+<style lang="sass" src="./assets/style/main.sass"></style>
